6e4dd08c5a9d694309e71c239057b33af45d9617
vinicius <viniaduarte@outlook.com>
2022-01-27 11:20:49 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
