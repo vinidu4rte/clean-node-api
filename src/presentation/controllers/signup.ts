1b45d15cb2904615728e245ef715991f574811c4
vinicius <viniaduarte@outlook.com>
2022-01-27 22:28:21 -0300

    refactor: create helper bad request to return when filds arent inserted

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,16 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const { email, password, passwordConfirmation } = httpRequest.body
     if (!email) {
-      return { statusCode: 400, body: new MissingParamError('Email') }
+      return badRequest(new MissingParamError('Email'))
     } else if (!password) {
-      return { statusCode: 400, body: new MissingParamError('Password') }
+      return badRequest(new MissingParamError('Password'))
     } else if (!passwordConfirmation) {
-      return { statusCode: 400, body: new MissingParamError('Password confirmation') }
+      return badRequest(new MissingParamError('Password confirmation'))
     } else {
       return { statusCode: 200, body: {} }
     }
