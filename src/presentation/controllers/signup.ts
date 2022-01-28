ae5910ef3b1ad3fda127449bc7147e6d53bb52ae
vinicius <viniaduarte@outlook.com>
2022-01-27 21:59:44 -0300

    feat: ensure email, password and pass confirm will be passed

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -13,6 +13,13 @@
 
 export class SignUpController {
   handle (httpRequest: any): any {
-    return { statusCode: 400, body: new Error('Missing param: email') }
+    const { email, password, passwordConfirmation } = httpRequest.body
+    if (!email) {
+      return { statusCode: 400, body: new Error('Missing param: email') }
+    } else if (!password) {
+      return { statusCode: 400, body: new Error('Missing param: password') }
+    } else if (!passwordConfirmation) {
+      return { statusCode: 400, body: new Error('Missing param: password confirmation') }
+    }
   }
 }
