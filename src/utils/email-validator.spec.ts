09166237dfc7e2d20dec2e6bc8d32462d8271c0e
vinicius <viniaduarte@outlook.com>
2022-01-31 17:49:19 -0300

    test: mock validator lib instead of use the real lib

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,5 +1,12 @@
 import { EmailValidator } from '../presentation/protocols/email-validator'
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('Email Validator Adapter', () => {
   const makeSut = (): EmailValidator => {
@@ -9,7 +16,19 @@ describe('Email Validator Adapter', () => {
 
   test('Should return false if validator returns false', () => {
     const sut = makeSut()
-    const result = sut.isValid('invalid_email')
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
+    const result = sut.isValid('invalid_email@email.com')
     expect(result).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+
+    const result = sut.isValid('valid_email@email.com')
+    expect(result).toBe(true)
   })
 })
