71295e8532fc817a8f6cb3fc8ccb904678292b10
vinicius <viniaduarte@outlook.com>
2022-01-31 23:27:30 -0300

    feat: ensure Encrypter will receive password

---
+++ src/data/db-add-account.spec.ts
@@ -0,0 +1,31 @@
+import { Encrypter } from './protocols/encrypter'
+import { DbAddAccount } from './db-add-account'
+
+describe('DbAddAccount', () => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (password: string): Promise<string> {
+      return new Promise(resolve => resolve('hash_pass'))
+    }
+  }
+
+  const makeSut = (): any => {
+    const encrypterStub = new EncrypterStub()
+    const sut = new DbAddAccount(encrypterStub)
+    return {
+      sut,
+      encrypterStub
+    }
+  }
+
+  test('Ensure encrypter will receive password', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encrypt = jest.spyOn(encrypterStub, 'encrypt')
+    const account = {
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+
+    await sut.add(account)
+    expect(encrypt).toHaveBeenCalledWith('valid_password')
+  })
+})
