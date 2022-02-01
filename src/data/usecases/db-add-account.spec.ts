638a8765ba69a707d1109477aa6596c7e89e1648
vinicius <viniaduarte@outlook.com>
2022-02-01 15:09:25 -0300

    feat: ensure AddAccountRepo will receive correct values

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -1,5 +1,8 @@
+import { AccountModel } from '../../domain/models/account'
 import { Encrypter } from '../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+import { AddAccountModel } from '../../domain/usecases/add-account'
+import { AddAccountRepo } from '../protocols/add-account-repo'
 
 describe('DbAddAccount', () => {
   const makeEncrypter = (): Encrypter => {
@@ -11,18 +14,39 @@ describe('DbAddAccount', () => {
     return new EncrypterStub()
   }
 
-  const makeSut = (): any => {
+  const makeAddAccountRepo = (): AddAccountRepo => {
+    class AddAccountRepo implements AddAccountRepo {
+      async add (account: AddAccountModel): Promise<AccountModel> {
+        return new Promise(resolve => resolve({
+          id: 'valid_id',
+          email: 'valid_email',
+          password: 'hash_pass'
+        }))
+      }
+    }
+    return new AddAccountRepo()
+  }
+
+  interface SutTypes {
+    sut: DbAddAccount
+    encrypterStub: Encrypter
+    addAccountRepoStub: AddAccountRepo
+  }
+
+  const makeSut = (): SutTypes => {
     const encrypterStub = makeEncrypter()
-    const sut = new DbAddAccount(encrypterStub)
+    const addAccountRepoStub = makeAddAccountRepo()
+    const sut = new DbAddAccount(encrypterStub, addAccountRepoStub)
     return {
       sut,
-      encrypterStub
+      encrypterStub,
+      addAccountRepoStub
     }
   }
 
   test('Ensure encrypter will receive password', async () => {
-    const { sut } = makeSut()
-    const encryptSpy = jest.spyOn(sut.encrypter, 'encrypt')
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const account = {
       email: 'valid_email@email.com',
       password: 'valid_password'
@@ -33,8 +57,8 @@ describe('DbAddAccount', () => {
   })
 
   test('Ensure DbAddAccount throw error if encrypter throws', async () => {
-    const { sut } = makeSut()
-    jest.spyOn(sut.encrypter, 'encrypt').mockImplementationOnce(async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockImplementationOnce(async () => {
       return new Promise((resolve, reject) => reject(new Error()))
     })
 
@@ -45,5 +69,21 @@ describe('DbAddAccount', () => {
 
     const promise = sut.add(account)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Ensure AddAccountRepo will receive correct values', async () => {
+    const { sut, addAccountRepoStub } = makeSut()
+    const addAccountRepoSpy = jest.spyOn(addAccountRepoStub, 'add')
+
+    const account = {
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+
+    await sut.add(account)
+    expect(addAccountRepoSpy).toHaveBeenCalledWith({
+      email: 'valid_email@email.com',
+      password: 'hash_pass'
+    })
   })
 })
--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,21 +1,26 @@
 import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
 import { AccountModel } from '../../domain/models/account'
 import { Encrypter } from '../protocols/encrypter'
+import { AddAccountRepo } from '../protocols/add-account-repo'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepo: AddAccountRepo
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepo: AddAccountRepo) {
     this.encrypter = encrypter
+    this.addAccountRepo = addAccountRepo
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    const hash = await this.encrypter.encrypt(account.password)
+    const hashedPassword = await this.encrypter.encrypt(account.password)
 
-    return new Promise(resolve => resolve({
-      id: 'string',
-      email: 'string',
-      password: hash
-    }))
+    // não mexer no objeto que chega
+    // account.password = hashedPassword
+
+    // utilizar o assign faz uma copia do objeto e podemos modificar um valor que existe nele sobreescrevendo-o.
+    const accountModel = await this.addAccountRepo.add(Object.assign({}, account, { password: hashedPassword }))
+
+    return new Promise(resolve => resolve(accountModel))
   }
 }
